2152a8cd8151a76341b2e8dd3cc2175575d747cc
luizmarcelloxamarin <luiztelemicro@hotmail.com>
2020-08-24 20:15:46 -0300

    24.08.2020_20:15hs

---
+++ App_MessageCenter/App_MessageCenter/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using App_MessageCenter.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_MessageCenter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new ExemploMessagePage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ App_MessageCenter/App_MessageCenter/Views/ExemploMessagePage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_MessageCenter.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExemploMessagePage : ContentPage
+    {
+        private List<string> _listaEventoTempo;
+
+        private bool _assinouMensagem = false;
+
+        public ExemploMessagePage()
+        {
+            InitializeComponent();
+            _listaEventoTempo = new List<string>();
+        }
+
+        private void btnLimpar_Clicked(object sender, EventArgs e)
+        {
+            _listaEventoTempo = new List<string>();
+            AtualizaLista();
+        }
+
+        private void AtualizaLista()
+        {
+            lvwEventos.ItemsSource = new ObservableCollection<string>(_listaEventoTempo);
+        }
+
+        private void btnPublicar_Clicked(object sender, EventArgs e)
+        {
+            //Publicando a mensagem(método "Send()")
+            //(quem é o remetente(que publica),string label que identifica a mensagem, informação a ser passada)
+            MessagingCenter.Send(this, "datahora", DateTime.Now);
+        }
+
+        private void btnSubscrever_Clicked(object sender, EventArgs e)
+        {
+            _assinouMensagem = !_assinouMensagem;
+
+            if (_assinouMensagem)
+            {
+                btnSubscrever.Text = "Cancelar Assinatura";
+
+                //Assinando a mensagem(método "Subscribe()")
+                //(quem vai assinar,string etiqueta da mensagem)
+                //<remetente da mensagem(própria página),um argumento(a mansagem)>
+                MessagingCenter.Subscribe<ExemploMessagePage, DateTime>(this, "datahora",
+                    (pagina, hora) =>
+                    {
+                        _listaEventoTempo.Add(hora.ToString());
+                        AtualizaLista();
+                    });
+            }
+            else
+            {
+                //Cancelando a assinatura(Método "Unsubscribe()")
+                //<Quem tá cancelando a assinatura(própria página), um argumento(a mensagem)>
+                MessagingCenter.Unsubscribe<ExemploMessagePage, DateTime>(this, "datahora");
+            }
+        }
+
+
+    }
+}
